310f80aa1f8f6f3e4639c4766394a54630460996
s-andrews <simon.andrews@babraham.ac.uk>
2016-12-21 11:28:16 +0000

    Bumped version to 1.37.0.devel since there's so much new functionality added now.

--- uk/ac/babraham/SeqMonk/SeqMonkApplication.java
+++ uk/ac/babraham/SeqMonk/SeqMonkApplication.java
@@ -82,7 +82,7 @@ public class SeqMonkApplication extends JFrame implements ProgressListener, Data
 	private static SeqMonkApplication application;
 	
 	/** The version of SeqMonk */
-	public static final String VERSION = "1.36.1.devel";
+	public static final String VERSION = "1.37.0.devel";
 	
 	private SeqMonkMenu menu;
 	
