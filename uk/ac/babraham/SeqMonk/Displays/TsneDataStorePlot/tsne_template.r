e80b5ad3efe8b33922ea0dbd1dfa875414fd5f98
s-andrews <simon@proteo.me.uk>
2017-05-10 12:00:14 +0100

    Fixed store name typo in tsne template

--- uk/ac/babraham/SeqMonk/Displays/TsneDataStorePlot/tsne_template.r
+++ uk/ac/babraham/SeqMonk/Displays/TsneDataStorePlot/tsne_template.r
@@ -5,7 +5,7 @@ library(tsne)
 # Read in the full data table from SeqMonk
 tsne.data <- read.delim("data.txt",quote="",header=FALSE)
 
-tsne(t(pca.data), perplexity=%%PERPLEXITY%%, min_cost=%%CUTOFF%%, epoch=10) -> tsne.results
+tsne(t(tsne.data), perplexity=%%PERPLEXITY%%, min_cost=%%CUTOFF%%, epoch=10) -> tsne.results
 
 # Write the TSNE data to a file
 write.table(tsne.results,file="tsne_data.txt",row.names=TRUE,col.names=NA,quote=FALSE,sep="\t")
