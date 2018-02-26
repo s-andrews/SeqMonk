d24165e0154e00516bc6995615a017b959e3e10f
s-andrews <simon.andrews@babraham.ac.uk>
2018-02-26 09:55:39 +0000

    Add null check when creating clusters

--- uk/ac/babraham/SeqMonk/DataTypes/Cluster/ClusterPair.java
+++ uk/ac/babraham/SeqMonk/DataTypes/Cluster/ClusterPair.java
@@ -34,6 +34,14 @@ public class ClusterPair {
 	}
 	
 	public ClusterPair (ClusterPair pair1, ClusterPair pair2, float rValue) {
+		
+		// We had some crash reports where one of the clusters must have been
+		// null. Never got to the cause so added this trap to try to get a better
+		// stack trace if it happens again.
+		if (pair1 == null || pair2 == null) {
+			throw new IllegalStateException("Clusters cannot be null");
+		}
+		
 		this.pair1 = pair1;
 		this.pair2 = pair2;
 		this.rValue = rValue;
