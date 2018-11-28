292bc7cbb88ec647d1289dab2c019232647569e7
s-andrews <simon.andrews@babraham.ac.uk>
2018-11-28 17:14:23 +0000

    Added column pasting and sorting in bulk dataset rename. Fixes #147

--- uk/ac/babraham/SeqMonk/Dialogs/DataSetEditor/MultiDataSetRenameDialog.java
+++ uk/ac/babraham/SeqMonk/Dialogs/DataSetEditor/MultiDataSetRenameDialog.java
@@ -1,11 +1,16 @@
 package uk.ac.babraham.SeqMonk.Dialogs.DataSetEditor;
 
 import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -34,6 +39,22 @@ public class MultiDataSetRenameDialog extends JDialog {
 		nameTable = new JTable(new renameTableModel());
 		nameTable.setColumnSelectionAllowed(true);
 		nameTable.setRowSelectionAllowed(false);
+		nameTable.setAutoCreateRowSorter(true);
+		nameTable.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.isControlDown() && e.getKeyCode()==KeyEvent.VK_V) {
+					pasteFromClipboard();
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {}
+		});
 		
 		getContentPane().add(new JScrollPane(nameTable), BorderLayout.CENTER);
 		
@@ -77,6 +98,29 @@ public class MultiDataSetRenameDialog extends JDialog {
 	}
 	
 	
+	private void pasteFromClipboard() { 
+        int startRow=nameTable.getSelectedRows()[0]; 
+        int startCol=nameTable.getSelectedColumns()[0];
+        
+        if (startCol != 1) return;
+
+        String pasteString = ""; 
+        try { 
+                pasteString = (String)(Toolkit.getDefaultToolkit().getSystemClipboard().getContents(this).getTransferData(DataFlavor.stringFlavor)); 
+        } catch (Exception e) { 
+                JOptionPane.showMessageDialog(null, "Invalid Paste Type", "Invalid Paste Type", JOptionPane.ERROR_MESSAGE);
+                return; 
+        } 
+        
+        String[] lines = pasteString.split("\n"); 
+        for (int i=0 ; i<lines.length; i++) { 
+        	if (nameTable.getRowCount()>startRow+i && nameTable.getColumnCount()>startCol) { 
+        		nameTable.setValueAt(lines[i], startRow+i, startCol);
+        	} 
+        }
+	} 
+
+	
 	private class renameTableModel extends AbstractTableModel {
 
 		@Override
@@ -110,6 +154,7 @@ public class MultiDataSetRenameDialog extends JDialog {
 		public void setValueAt(Object o,int r, int c) {
 			if (c==1) {
 				names[r] = (String)o;
+				fireTableCellUpdated(r, c);
 			}
 
 		}
