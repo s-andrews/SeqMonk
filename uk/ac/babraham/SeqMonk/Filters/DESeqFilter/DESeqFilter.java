83cbb19f4733d87934393b9cea92b44777d38c8c
s-andrews <simon.andrews@babraham.ac.uk>
2018-11-28 12:18:02 +0000

    Make DESeq report p,q and fold change

--- uk/ac/babraham/SeqMonk/Filters/DESeqFilter/DESeqFilter.java
+++ uk/ac/babraham/SeqMonk/Filters/DESeqFilter/DESeqFilter.java
@@ -281,7 +281,7 @@ public class DESeqFilter extends ProbeFilter {
 
 			ProbeList newList;
 
-			newList = new ProbeList(startingList,"","",new String[] {"P-value","FDR","Fold Change"});
+			newList = new ProbeList(startingList,"","",new String[] {"P-value","FDR","Log2 Fold Change"});
 
 			File hitsFile = new File(tempDir.getAbsolutePath()+"/hits.txt");
 
@@ -291,13 +291,11 @@ public class DESeqFilter extends ProbeFilter {
 
 			while ((line = br.readLine()) != null) {
 				String [] sections = line.split("\t");
-
 				
-				// TODO: FIX THIS - need to check the actual format and rewrite accordingly
 				int probeIndex = Integer.parseInt(sections[0]);
-				float pValue = Float.parseFloat(sections[sections.length-1]);
-				float qValue = Float.parseFloat(sections[sections.length-2]);
-				float foldChange = Float.parseFloat(sections[sections.length-3]);
+				float pValue = Float.parseFloat(sections[5]);
+				float qValue = Float.parseFloat(sections[6]);
+				float foldChange = Float.parseFloat(sections[2]);
 				newList.addProbe(probes[probeIndex],new float[]{pValue,qValue,foldChange});
 			}
 
