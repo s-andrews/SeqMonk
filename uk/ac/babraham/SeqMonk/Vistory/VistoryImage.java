dc72072a801190d27777b0d7b0520819e15d7923
s-andrews <simon.andrews@babraham.ac.uk>
2018-12-10 12:03:06 +0000

    Link vistory to the image saver

--- uk/ac/babraham/SeqMonk/Vistory/VistoryImage.java
+++ uk/ac/babraham/SeqMonk/Vistory/VistoryImage.java
@@ -1,19 +1,31 @@
 package uk.ac.babraham.SeqMonk.Vistory;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JPanel;
 
-public class VistoryImage implements VistoryBlock {
+public class VistoryImage extends JPanel implements VistoryBlock {
 
+	private BufferedImage image;
+	
+	public VistoryImage (BufferedImage image) {
+		this.image = image;
+	}
+	
 	@Override
 	public JPanel getPanel() {
-		// TODO Auto-generated method stub
-		return null;
+		return(this);
 	}
 
 	@Override
 	public String getHTML() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void paintComponent (Graphics g) {
+		g.drawImage(image, 0, 0, this);
 	}
 
 }
