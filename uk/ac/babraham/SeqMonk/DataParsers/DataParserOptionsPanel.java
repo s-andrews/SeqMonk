7bb72b222e23be840e313047a3eadac3652b106a
s-andrews <simon.andrews@babraham.ac.uk>
2019-05-31 20:10:15 +0100

    Don't add pair distance if importing spliced data. Fixes #185

--- uk/ac/babraham/SeqMonk/DataParsers/DataParserOptionsPanel.java
+++ uk/ac/babraham/SeqMonk/DataParsers/DataParserOptionsPanel.java
@@ -339,7 +339,7 @@ public class DataParserOptionsPanel extends JPanel implements ItemListener, KeyL
 			sb.append(extendReads());
 		}
 		
-		if (readType != null && pairedEnd()) {
+		if (readType != null && pairedEnd() && (splitSplicedReads != null && splitSplicedReads())) {
 			if (pairDistanceCutoff() != 0) {
 				sb.append("Paired distance cutoff = ");
 				sb.append(pairDistanceCutoff());
