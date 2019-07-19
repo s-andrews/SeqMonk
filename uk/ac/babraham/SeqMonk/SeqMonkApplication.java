57ccbf32d71807ebe3be0cb9e3e6f8cd52298b71
s-andrews <simon.andrews@babraham.ac.uk>
2019-07-19 14:01:24 +0100

    Bump version to 1.45.4.devel

--- uk/ac/babraham/SeqMonk/SeqMonkApplication.java
+++ uk/ac/babraham/SeqMonk/SeqMonkApplication.java
@@ -87,7 +87,7 @@ public class SeqMonkApplication extends JFrame implements ProgressListener, Data
 	private static SeqMonkApplication application;
 	
 	/** The version of SeqMonk */
-	public static final String VERSION = "1.45.3";
+	public static final String VERSION = "1.45.4.devel";
 	
 	private SeqMonkMenu menu;
 	
