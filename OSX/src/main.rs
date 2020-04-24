1ba816ca8e558a551c0a4dfeb9b2a18075bb70e2
Simon Andrews <simon.andrews@babraham.ac.uk>
2020-04-24 17:41:19 +0100

    Changed path in osxlauncher

--- OSX/src/main.rs
+++ OSX/src/main.rs
@@ -6,7 +6,7 @@ use std::process;
 fn main() {
     let argv: Vec<String> = env::args().skip(1).collect();
 
-    let err = exec::Command::new("seqmonk").args(&argv).exec();
+    let err = exec::Command::new("./seqmonk").args(&argv).exec();
 
     println!("Error launching seqmonk: {}",err);
     process::exit(1);
