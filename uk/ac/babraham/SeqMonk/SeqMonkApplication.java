00c8abcb06c246ed58e3d0d07d11f64184843deb
Simon Andrews <simon@proteo.me.uk>
2022-03-22 11:30:40 +0000

    Bump version to 1.48.1 and update release notes

--- uk/ac/babraham/SeqMonk/SeqMonkApplication.java
+++ uk/ac/babraham/SeqMonk/SeqMonkApplication.java
@@ -91,7 +91,7 @@ public class SeqMonkApplication extends JFrame implements ProgressListener, Data
 	private static SeqMonkApplication application;
 	
 	/** The version of SeqMonk */
-	public static final String VERSION = "1.48.0";
+	public static final String VERSION = "1.48.1";
 	
 	private SeqMonkMenu menu;
 	
