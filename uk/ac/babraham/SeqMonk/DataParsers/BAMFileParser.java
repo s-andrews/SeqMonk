878bbd1db3bd2435503fe07bd9858ec20e03190f
Simon Andrews <simon.andrews@babraham.ac.uk>
2025-05-07 19:09:09 +0100

    Fix single import in importer

--- uk/ac/babraham/SeqMonk/DataParsers/BAMFileParser.java
+++ uk/ac/babraham/SeqMonk/DataParsers/BAMFileParser.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Vector;
 
 import javax.swing.JPanel;
