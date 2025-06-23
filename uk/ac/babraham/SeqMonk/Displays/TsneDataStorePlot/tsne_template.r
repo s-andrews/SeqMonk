f00f651e20504a0a30b56e2067492c817e7085c6
Simon Andrews <simon.andrews@babraham.ac.uk>
2025-06-23 12:09:44 +0100

    Add check_duplicates=FALSE to Rtsne.
    Fixes #285

--- uk/ac/babraham/SeqMonk/Displays/TsneDataStorePlot/tsne_template.r
+++ uk/ac/babraham/SeqMonk/Displays/TsneDataStorePlot/tsne_template.r
@@ -5,7 +5,7 @@ library(Rtsne)
 # Read in the full data table from SeqMonk
 tsne.data <- read.delim("data.txt",quote="",header=FALSE)
 
-Rtsne(t(tsne.data), perplexity=%%PERPLEXITY%%, max_iter=%%ITERATIONS%%, verbose=TRUE) -> tsne.results
+Rtsne(t(tsne.data), perplexity=%%PERPLEXITY%%, max_iter=%%ITERATIONS%%, verbose=TRUE, check_duplicates=FALSE) -> tsne.results
 
 # Write the TSNE data to a file
 write.table(tsne.results$Y,file="tsne_data.txt",row.names=TRUE,col.names=NA,quote=FALSE,sep="\t")
